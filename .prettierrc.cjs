e086efa121423dfe8818b867e574ec2a415633cf
kris <hithard5@gmail.com>
2024-02-17 19:26:07 +0100

    cleanup

---
+++ .prettierrc.cjs
@@ -0,0 +1,4 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  tabWidth: 2,
+};
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function App() {
+  return <h1>KNUR + REACT</h1>;
+}
+
+export { App };
