882d9b964621a42dc31c6f05dc544c6d04cab6af
postmahone <oliwer.kirkiewicz@gmail.com>
2024-02-19 21:35:35 +0100

    poprawiono if'y

--- src/components/Zadanie5.tsx
+++ src/components/Zadanie5.tsx
@@ -7,36 +7,36 @@ export const Zadanie5 = () => {
     const [longitude, setLongitude] = useState(13.41);
     const linkDoAPI = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
     const changeLatitude = (e) => {
-        if (e.target.value !== "") setLatitude(e.target.value)
-        else if (e.target.value * 1 > 90) setLatitude(90);
-        else if (e.traget.value*1 < 90) setLatitude(-90);
-        else {
-    setLatitude(oldge=>oldge)
-}
+        if (e.target.value === "") setLatitude(0);
+        else if (e.target.value*1 > -90 && e.target.value*1<90) setLatitude(e.target.value);
+        else if (e.target.value* 1 > 90) setLatitude(90);
+        else if (e.target.value*1 < -90) setLatitude(-90);
     }
     const changeLongitude = (e) => {
-        setLongitude(e.target.value);
+        if (e.target.value === "") setLongitude(0);
+        else if (e.target.value*1 > -180 && e.target.value*1<180) setLongitude(e.target.value);
+        else if (e.target.value* 1 > 180) setLongitude(180);
+        else if (e.target.value*1 < -180) setLongitude(-180);
     }
     const mujFetchBozy = async () => {
+    
         const fetchedData = await fetch(linkDoAPI);
         const formatedData = await fetchedData.json();
-        console.log("pobrano dane z API");
-        console.log(formatedData);
         setDane(formatedData);
+       
+       
     }
         useEffect(() =>{
-        const interwał = setInterval(mujFetchBozy, 6*1000)
+        const interwał = setInterval(mujFetchBozy, 60*1000)
         return () => {
             clearInterval(interwał);
         }
     },[latitude, longitude])
 
         useEffect(()=>{
-            console.log('wykonuje sie');
             (async()=>{mujFetchBozy()})()
         },[])
          useEffect(()=>{
-            console.log('wykonuje sie');
             (async()=>{mujFetchBozy()})()
         },[latitude, longitude])
     return(
@@ -47,10 +47,9 @@ export const Zadanie5 = () => {
       <input type="number" placeholder="ustaw latitude od -90 do 90" value={latitude} onChange={changeLatitude}/>
         <p>Longitude:</p>
       <input type="number" placeholder="ustaw longitude od -180 do 180" value={longitude} onChange={changeLongitude}/>
-      <p>{latitude}, {longitude}</p>
-      <div>Aktualna temperatura: {dane === false ? "pobieram dane..." : dane.current.temperature_2m}{dane === false ? null : dane.current_units.temperature_2m}</div>
-      <div>Aktualna prękość wiatru: {dane === false ? "pobieram dane..." : dane.current.wind_speed_10m}{dane === false ? null : dane.current_units.wind_speed10m}</div>
-      <div>Aktualna godzina: {dane === false ? "pobieram dane..." : dane.current.time}</div>
+      <div>Aktualna temperatura: {dane === false ? "pobieram dane..." : dane?.current.temperature_2m}{dane === false ? null : dane?.current_units.temperature_2m}</div>
+      <div>Aktualna prękość wiatru: {dane === false ? "pobieram dane..." : dane?.current.wind_speed_10m}{dane === false ? null : dane?.current_units.wind_speed10m}</div>
+      <div>Aktualna godzina: {dane === false ? "pobieram dane..." : dane?.current.time}</div>
     </>
     )
 }
